ca1eedb635d031feaf0cd244a32a6e14caf599c0
yoonzky <xleftheaven4u@gmail.com>
2026-08-30 01:21:51 +0300

    cache v62

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sudoku-v61';
+const CACHE='sudoku-v62';
 const FILES=[
   './','./index.html','./manifest.webmanifest',
   './css/base.css','./css/home.css','./css/game.css','./css/modals.css','./css/mobile.css',
